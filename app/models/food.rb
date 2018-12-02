cd583a8f34ee10ed834724b2c9f924da8cf8b7d4
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-12-02 23:03:02 +0000

    Format food creation timestamps

--- app/models/food.rb
+++ app/models/food.rb
@@ -5,4 +5,8 @@ class Food < ActiveRecord::Base
   validates :name, uniqueness: { scope: :user_id, case_sensitive: false }
   validates :user_id, presence: true
   validates_associated :user
+
+  def when_fridged
+    DateTime.strptime(self.datetime_created.to_s, "%s").strftime("%Y-%m-%d %l:%M%P")
+  end
 end
